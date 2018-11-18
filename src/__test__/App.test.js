1a606fa2fa0a21d8c0ed104e87416d5396b30cdd
james-chege <jymeschege@gmail.com>
2018-11-18 16:40:04 +0300

    chore(testing-setup): setup testing
    - Setup enzyme and Jest for testing
    [FINISHES #162036465]

---
+++ src/__test__/App.test.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../components/App';
+
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+  expect(() => shallow(<App />)).not.toThrow();
+});
