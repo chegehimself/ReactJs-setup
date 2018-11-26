4a37e824ff5dbcc967932621e01a8224e540cf62
james-chege <jymeschege@gmail.com>
2018-11-26 16:31:12 +0300

    chore(eslint) Setup eslint
    - make sure that the airbnb linting style is setup

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude:/node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ],
       }
     ]
   },
